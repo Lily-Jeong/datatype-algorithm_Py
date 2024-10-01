04a6c71a38f15662a455fee4ee08eb2015c8828a
Lily Jeong <superfreak.co.kr@gmail.com>
2024-10-01 09:43:19 +0900

    ch01 review

--- ch01/hanoiTower.py
+++ ch01/hanoiTower.py
@@ -1,7 +1,8 @@
 def hanoi_tower(n, fr, tmp, to):
-    if(n == 1):
+    # n = 원판의 수 | fr = 시작 막대 | tmp = 임시 막대 | to = 목표 막대
+    if(n == 1): #순환 호출을 멈추는 부분. 원판이 하나라면 바로 이동.
         print("원판 1: %s --> %s" % (fr, to))
-    else:
-        hanoi_tower(n - 1, fr, to. tmp)
-        print("원판 %d: %s --> %s" % (n,fr,to))
-        hanoi_tower(n - 1, tmp, fr, to)
+    else: #순환호출 부분.
+        hanoi_tower(n - 1, fr, to. tmp) # 단계1
+        print("원판 %d: %s --> %s" % (n,fr,to)) # 단계2
+        hanoi_tower(n - 1, tmp, fr, to) # 단계3
